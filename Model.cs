5c440e8d9aa7e59f729695ce0f8fa0be11207c6e
Deymon <Deymon@DK>
2022-02-06 18:28:41 +0300

    Добавьте файлы проекта.

---
+++ Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace mvc
+{
+    class Model
+    {
+        public static float a = 0;
+        public static float b = 0;
+        public static float rezultat = 0;
+        public static List<string> DataList = new List<string> { "Умножение", "Сложение", "Деление", "Вычитание" };
+        public static int index = 0;
+    }
+}
+
